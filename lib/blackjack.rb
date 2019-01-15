85307482c3bc968d947ef3cc86ded24ec60c2c3b
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-15 19:27:17 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,51 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!" 
+  #end_game here
 end
+
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  cardtotal = deal_card + deal_card
+    display_card_total(cardtotal)
+    cardtotal
 end
 
 def invalid_command
-  # code invalid_command here
+ puts "Please enter a valid command"
+end
+
+
+def hit?(cardtotal)
+  prompt_user
+  input = get_user_input
+if input == 's'
+  cardtotal = cardtotal
+elsif input == 'h'
+  cardtotal += deal_card
+elsif input != 's' && input != 'h'
+  invalid_command
+  prompt_user
+ end
 end
 
 #####################################################
@@ -39,6 +53,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  cardtotal = initial_round 
+until cardtotal > 21
+cardtotal = hit?(cardtotal)
+display_card_total(cardtotal)
+  end
+end_game(cardtotal)
 end
     
